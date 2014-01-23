e861b44d5d0832bb7004b1b3f2b04ffb6134e73b
m.w.bonawitz <m.w.bonawitz@gmail.com>
2014-01-23 20:00:19 +0000

    Added NetworkedPlayer getting a move from the network

--- src/model/NetworkedPlayer.java
+++ src/model/NetworkedPlayer.java
@@ -1,9 +1,22 @@
 package model;
 
+import java.io.IOException;
+
 public class NetworkedPlayer extends Player {
 
 	public NetworkedPlayer(final Game game, final String name) {
-		super(game, name);
-	}	
+		super(game, name, false);
+	}
+
+	public Move getMove() {
+		String input = "";
+		try {
+			input = in.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new Move(input);
+	}
 
 }

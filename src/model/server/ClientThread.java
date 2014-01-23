9d90a3e7a4c732bd9ae1eee7470785c2ee4dde88
m.w.bonawitz <m.w.bonawitz@gmail.com>
2014-01-23 16:58:32 +0000

    Updated Server and ClientThread functionality

--- src/model/server/ClientThread.java
+++ src/model/server/ClientThread.java
@@ -8,20 +8,43 @@ import java.net.Socket;
 
 public class ClientThread extends Thread {
 	
+	private final Server server;
 	private final int id;
 	private PrintWriter out;
 	private BufferedReader in;
 	
-	public ClientThread(final int id, final Socket socket){
+	public ClientThread(final int id, final Socket socket, final Server server){
 		
+		this.server = server;
 		this.id = id;
 		try {
-			out = new PrintWriter(socket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			this.out = new PrintWriter(socket.getOutputStream(), true);
+			this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public int getID(){
+		return id;
+	}
+	
+	public void sendMessage(final String message){
+		out.println(message);
+	}
+	
+	@Override
+	public void run(){
+		String input; //what the client sends in		
+		try {
+			System.out.println("Waiting for message");
+			while((input = in.readLine()) != null){
+				server.sendMessageToAll(id, input);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/model/server/Server.java
+++ src/model/server/Server.java
@@ -7,36 +7,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Server extends Thread {
-	
+
+	private boolean accepting;
 	private int count;
 	private int port;
 	private List<ClientThread> threads;
-	
-	public Server(final int port){
+
+	public Server(final int port) {
+		this.accepting = true;
 		this.count = 0;
 		this.port = port;
 		this.threads = new ArrayList<ClientThread>();
 	}
-	
+
 	public void start() {
-		
-		try(ServerSocket serverSocket = new ServerSocket(port)){
-			
-			while(true){
+		waitForConnections();
+	}
+
+	public void waitForConnections() {
+		try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+			while (accepting) {
 				Socket clientSocket = serverSocket.accept();
 				count++;
-				threads.add(new ClientThread(count, clientSocket));
+				threads.add(new ClientThread(count, clientSocket, this));
 				threads.get(threads.size() - 1).start();
+				if(threads.size() == 2){
+					accepting = false;
+				}
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 	
-	public void tearDown(){
+	public void setAllowConnections(final boolean accepting){
+		this.accepting = accepting;
+	}
+	
+	public void sendMessageToAll(final int id, final String message){
 		
+		for(final ClientThread client : threads){
+			if(id != client.getID()){
+				client.sendMessage(message);
+			}
+		}
+		
+	}
+
+	public void tearDown() {
+		
+		for(final ClientThread t : threads){
+			//stop threads
+		}
+		threads.clear();
+
 	}
 
 }

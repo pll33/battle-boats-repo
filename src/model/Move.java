e9ba753150336b9de8c81a523c7e35e3c9b1322e
m.w.bonawitz <m.w.bonawitz@gmail.com>
2014-01-23 19:59:45 +0000

    Added serializing and deserializing a Move to and from a String

--- src/model/Move.java
+++ src/model/Move.java
@@ -8,4 +8,23 @@ public class Move {
 		this.x = x;
 		this.y = y;
 	}
+	
+	public Move(final String move){
+		Move m = parseMove(move);
+		this.x = m.x;
+		this.y = m.y;
+	}
+	
+	public String serializeToString(){
+		return "x:" + x + ",y:" + y;
+	}
+	
+	private Move parseMove(final String move){
+		String[] xAndY = move.split(",");
+		String[] xTemp = xAndY[0].split(":");
+		String[] yTemp = xAndY[1].split(":");
+		int x = Integer.parseInt(xTemp[1]);
+		int y = Integer.parseInt(yTemp[1]);
+		return new Move(x,y);
+	}
 }

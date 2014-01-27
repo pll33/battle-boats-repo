3c771345f15703498766f25ea43b00aaa76eccdb
m.w.bonawitz@gmail.com <m.w.bonawitz@gmail.com@9362bfa4-203c-4e84-de78-cb94c5e57fc3>
2014-01-27 23:50:43 +0000

    Edited source formatting

--- trunk/src/controller/GameController.java
+++ trunk/src/controller/GameController.java
@@ -7,27 +7,26 @@ import model.Game;
 public class GameController {
 
 	private Game game;
-	
-	public GameController(){
+
+	public GameController() {
 		ArrayList<Integer> sizes = new ArrayList<Integer>();
 		sizes.add(2);
 		sizes.add(3);
 		sizes.add(3);
 		sizes.add(4);
 		sizes.add(5);
-		game = new Game("Player 1", "Player2",10,10,sizes,false);
+		game = new Game("Player 1", "Player2", 10, 10, sizes, false);
 	}
-	
-	public GameController(String player1, String player2, int width, int height, 
-			ArrayList<Integer> boatSizes, boolean multiplayer) 
-{
-game = new Game(player1, player2, width, height, boatSizes, multiplayer);
-}
-	
-	public void startGame(){
+
+	public GameController(String player1, String player2, int width,
+			int height, ArrayList<Integer> boatSizes, boolean multiplayer) {
+		game = new Game(player1, player2, width, height, boatSizes, multiplayer);
+	}
+
+	public void startGame() {
 		boolean gameOn = true;
-		while(gameOn){
-			
+		while (gameOn) {
+
 		}
 	}
 }

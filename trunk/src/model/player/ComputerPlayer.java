96072eaf80e7d2fbaf8977a10789859a74fdcf29
m.w.bonawitz@gmail.com <m.w.bonawitz@gmail.com@9362bfa4-203c-4e84-de78-cb94c5e57fc3>
2014-01-28 17:12:52 +0000

    Changed Computer to connect to Server

--- trunk/src/model/player/ComputerPlayer.java
+++ trunk/src/model/player/ComputerPlayer.java
@@ -16,7 +16,7 @@ public class ComputerPlayer extends Player{
 	private static Random rand;
 	
 	public ComputerPlayer(Game game) {
-		super(game, "Computer", false);
+		super(game, "Computer");
 		int width = game.getWidth();
 		int height = game.getHeight();
 		

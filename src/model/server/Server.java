307279cc1b95f101edbc1030009632b60aa07c14
m.w.bonawitz <m.w.bonawitz@gmail.com>
2014-01-28 17:13:28 +0000

    Changed start method to run for proper Threading

--- src/model/server/Server.java
+++ src/model/server/Server.java
@@ -5,6 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 public class Server extends Thread {
 
@@ -12,27 +13,36 @@ public class Server extends Thread {
 	private int count;
 	private int port;
 	private List<ClientThread> threads;
+	private Semaphore mutex;
 
-	public Server(final int port) {
+	public Server(final int port, final Semaphore mutex) {
 		this.accepting = true;
 		this.count = 0;
 		this.port = port;
 		this.threads = new ArrayList<ClientThread>();
+		this.mutex = mutex;
 	}
 
-	public void start() {
+	@Override
+	public void run() {
+		System.out.println("Server Started");
 		waitForConnections();
 	}
 
 	public void waitForConnections() {
 		try (ServerSocket serverSocket = new ServerSocket(port)) {
 
+			System.out.println("Accepting Connections");
+			mutex.release(); //server has started
+			
 			while (accepting) {
 				Socket clientSocket = serverSocket.accept();
+				System.out.println("Client Connected");
 				count++;
 				threads.add(new ClientThread(count, clientSocket, this));
 				threads.get(threads.size() - 1).start();
 				
+				System.out.println("Number of clients: " + threads.size());
 				//two players have joined, so stop accepting connections
 				if(threads.size() == 2){
 					accepting = false;

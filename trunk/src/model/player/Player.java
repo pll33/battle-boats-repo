e486cd344649fbffaf8841aedbfc7ad58b01088d
m.w.bonawitz@gmail.com <m.w.bonawitz@gmail.com@9362bfa4-203c-4e84-de78-cb94c5e57fc3>
2014-01-28 20:17:15 +0000

    added logging and added input/output streams (after temporary remove)

--- trunk/src/model/player/Player.java
+++ trunk/src/model/player/Player.java
@@ -38,19 +38,23 @@ public abstract class Player {
 		playerName = name;
 
 		if (connectToServer == true) {
-			System.out.println("Player " + name + " attempting to connect to server");
+			logMessage("attempting to connect to server");
 			// will have to change this from being hardcoded
 			String ip = "127.0.0.1";
 			int port = 8080;
 
 			try {
 				this.socket = new Socket(ip, port);
-				//this.out = new ObjectOutputStream(socket.getOutputStream());
-				//this.in = new ObjectInputStream(socket.getInputStream());
+				this.out = new ObjectOutputStream(socket.getOutputStream());
+				this.in = new ObjectInputStream(socket.getInputStream());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	protected void logMessage(final String message){
+		System.out.println(playerName + ": " + message);
 	}
 
 	public boolean placeBoats(final ArrayList<Boat> boats) {

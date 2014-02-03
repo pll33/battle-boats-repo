30b616cc5d644a83fcc3cd4bb4243e5c0b3bc9e9
m.w.bonawitz@gmail.com <m.w.bonawitz@gmail.com@9362bfa4-203c-4e84-de78-cb94c5e57fc3>
2014-02-03 21:15:30 +0000

    Added Javadoc comments

--- trunk/src/controller/GameController.java
+++ trunk/src/controller/GameController.java
@@ -6,8 +6,14 @@ import model.Game;
 
 public class GameController {
 
+	/**
+	 * The game.
+	 */
 	private Game game;
 
+	/**
+	 * GameController constructor that creates a Game with default settings.
+	 */
 	public GameController() {
 		ArrayList<Integer> sizes = new ArrayList<Integer>();
 		sizes.add(2);
@@ -18,11 +24,23 @@ public class GameController {
 		game = new Game("Player 1", "Player2", 10, 10, sizes, false);
 	}
 
+	/**
+	 * GameController constructor that creates a Game with supplied settings.
+	 * @param player1 Player 1's name
+	 * @param player2 Player 2' name
+	 * @param width The width of the game board
+	 * @param height The height of the game board
+	 * @param boatSizes A List<Integer> indicating how many boats and their respective sizes.
+	 * @param multiplayer Whether this game is two humans players (true) or against computer (false)
+	 */
 	public GameController(String player1, String player2, int width,
 			int height, ArrayList<Integer> boatSizes, boolean multiplayer) {
 		game = new Game(player1, player2, width, height, boatSizes, multiplayer);
 	}
 
+	/**
+	 * The main game loop
+	 */
 	public void startGame() {
 		boolean gameOn = true;
 		while (gameOn) {

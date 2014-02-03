9b4f56c30a3e1448a0ef8f31f06032c41f2c1ce2
m.w.bonawitz@gmail.com <m.w.bonawitz@gmail.com@9362bfa4-203c-4e84-de78-cb94c5e57fc3>
2014-02-03 21:15:40 +0000

    Added Javadoc comments

--- trunk/src/core/MoveState.java
+++ trunk/src/core/MoveState.java
@@ -1,5 +1,9 @@
 package core;
 
+/**
+ * A enum of the possibles results of a move (firing a missing at the enemy).
+ *
+ */
 public enum MoveState {
 
 	HIT,MISS,INVALID
--- trunk/src/core/Orientation.java
+++ trunk/src/core/Orientation.java
@@ -1,5 +1,9 @@
 package core;
 
+/**
+ * An enum of the possible orientations that ships may be placed.
+ *
+ */
 public enum Orientation {
 	HORIZONTAL,
 	VERTICAL

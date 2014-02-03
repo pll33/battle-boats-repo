0274b09761125776068f9f6c0cc4cbcd69bd5d50
m.w.bonawitz@gmail.com <m.w.bonawitz@gmail.com@9362bfa4-203c-4e84-de78-cb94c5e57fc3>
2014-02-03 21:16:25 +0000

    Added Javadoc comments

--- trunk/src/model/server/ClientThread.java
+++ trunk/src/model/server/ClientThread.java
@@ -5,14 +5,43 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+/**
+ * A Thread spawned by Server to handle communication between multiple clients.
+ *
+ */
 public class ClientThread extends Thread {
 	
+	/**
+	 * The Server that spawned this thread.
+	 */
 	private final Server server;
+	
+	/**
+	 * The ID of this thread
+	 */
 	private final int id;
+	
+	/**
+	 * A stream used for sending messages (Objects) to this client.
+	 */
 	private ObjectOutputStream out;
+	
+	/**
+	 * A steam used for receiving messages (Objects) from other clients.
+	 */
 	private ObjectInputStream in;
+	
+	/**
+	 * A boolean indicating if this Thread is running.
+	 */
 	private boolean running;
 	
+	/**
+	 * Construct a new ClientThread.
+	 * @param id The ID of the Thread
+	 * @param socket The socket created for this Thread.
+	 * @param server The Server instance.
+	 */
 	public ClientThread(final int id, final Socket socket, final Server server){
 		
 		this.running = true;
@@ -29,10 +58,18 @@ public class ClientThread extends Thread {
 		
 	}
 	
+	/**
+	 * 
+	 * @return The ID of the Thread
+	 */
 	public int getID(){
 		return id;
 	}
 	
+	/**
+	 * Sends a message to this Client.
+	 * @param message The message to send.
+	 */
 	public void sendMessage(final String message){
 		try {
 			out.writeObject(message);
@@ -42,11 +79,18 @@ public class ClientThread extends Thread {
 		}
 	}
 	
+	/**
+	 * Change if this Thread is running.
+	 * @param running
+	 */
 	public void setRunning(final boolean running){
 		this.running = running;
 	}
 	
-	@Override
+	/**
+	 * The Threads main run method.
+	 */
+	@Override	
 	public void run(){
 		String input; //what the client sends in		
 		try {			
--- trunk/src/model/server/Server.java
+++ trunk/src/model/server/Server.java
@@ -7,12 +7,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Semaphore;
 
+/**
+ * A Thread used to recieve connections from Clients and spawn Threads to handle their requests.
+ *
+ */
 public class Server extends Thread {
 
+	/**
+	 * Whether the Server is accepting new connections
+	 */
 	private boolean accepting;
+	
+	/**
+	 * The count of how many Clients have connected since the Server has started
+	 */
 	private int count;
+	
+	/**
+	 * The port the Server will utilize.
+	 */
 	private int port;
+	
+	/**
+	 * A List of all the active ClientThreads.
+	 */
 	private List<ClientThread> threads;
+	
+	/**
+	 * A lock that gets released once the Server can recieve connections.
+	 */
 	private Semaphore mutex;
 
 	public Server(final int port, final Semaphore mutex) {
@@ -24,11 +47,17 @@ public class Server extends Thread {
 	}
 
 	@Override
+	/**
+	 * The Threads main run method
+	 */
 	public void run() {
 		logMessage("Server Started");
 		waitForConnections();
 	}
 
+	/**
+	 * Waits for connections from clients while <code>accepting = true</code>. <code>Accepting</code> becomes false in this method when 2 Clients have connected.
+	 */
 	public void waitForConnections() {
 		try (ServerSocket serverSocket = new ServerSocket(port)) {
 
@@ -54,14 +83,27 @@ public class Server extends Thread {
 		}
 	}
 	
+	/**
+	 * A utility function to log messages from the Server.
+	 * @param message The message to log.
+	 */
 	private void logMessage(final String message){
 		System.out.println("Server: " + message);
 	}
 	
+	/**
+	 * Change if the Server is accepting connections.
+	 * @param accepting
+	 */
 	public void setAllowConnections(final boolean accepting){
 		this.accepting = accepting;
 	}
 	
+	/**
+	 * Sends a message to all connected clients, except the client who originated the message. This method is useful for one client to send a message to all other clients.
+	 * @param id The ID of the client that originated the message.
+	 * @param message The message to send
+	 */
 	public void sendMessageToAll(final int id, final String message){
 		
 		for(final ClientThread client : threads){
@@ -72,6 +114,9 @@ public class Server extends Thread {
 		
 	}
 
+	/**
+	 * The Server's deconstructor
+	 */
 	public void tearDown() {
 		
 		//setAllowConnections(false);

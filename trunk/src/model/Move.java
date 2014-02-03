366f43a77beab72013d309dfaa1766f2f12cd99e
m.w.bonawitz@gmail.com <m.w.bonawitz@gmail.com@9362bfa4-203c-4e84-de78-cb94c5e57fc3>
2014-02-03 21:50:25 +0000

    Added Javadoc comments

--- trunk/src/model/Move.java
+++ trunk/src/model/Move.java
@@ -2,11 +2,29 @@ package model;
 
 import java.io.Serializable;
 
+/**
+ * A class representing a move (x and y coordinates) on a Board. This class is also Serializable so it can be sent over a network.
+ *
+ */
 public class Move implements Serializable {
 
 	private static final long serialVersionUID = -3552137844787073900L;
+	
+	/**
+	 * The x coordinate
+	 */
 	public int x;
+	
+	/**
+	 * the y coordinate
+	 */
 	public int y;
+	
+	/**
+	 * Create a move
+	 * @param x x coordinate
+	 * @param y y coordinate
+	 */
 	public Move(int x, int y){
 		this.x = x;
 		this.y = y;

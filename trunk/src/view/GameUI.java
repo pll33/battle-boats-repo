e9c4b8a812521f3ca10b21972f6fb736c19fb752
peterljr888@gmail.com <peterljr888@gmail.com@9362bfa4-203c-4e84-de78-cb94c5e57fc3>
2014-03-02 05:17:05 +0000

    Updated GameUI

--- trunk/src/view/GameUI.java
+++ trunk/src/view/GameUI.java
@@ -2,19 +2,29 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 import javax.swing.Timer;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
+import model.Board;
 import model.Game;
 
 import controller.GameController;
@@ -23,13 +33,19 @@ public class GameUI extends JFrame {
 
 	private static final long serialVersionUID = 5892131800795551733L;
 	private final Container contentPane;
-	private CardLayout contentPaneCL;
+	//private CardLayout contentPaneCL;
 	private JTextField timerLabel;
 	private Timer timer;
 	
 	private Game game;
 	private GameController gc;
+	
 	// consists of 2 boardUIs per player
+	private GameBoardUI playerBoardUI;
+	private GameBoardUI opposingBoardUI;
+	
+	private String playerName;
+	private String opponentName;
 	
 	public GameUI() {
 		contentPane = getContentPane();
@@ -39,15 +55,32 @@ public class GameUI extends JFrame {
 //		createComponents();
 	}
 	
-	public GameUI(Game g) {
-		this.game = g;
+	public GameUI(GameController gc, Board playerBoard) {
+		this.gc = gc;
+		this.game = gc.getGame();
+		this.playerName = game.getGameSettings().getPlayer1Name();
+		this.opponentName = game.getGameSettings().getPlayer2Name();
 		
-		setTitle("BattleBoats");
+		String matchTitle = playerName + " vs. " + opponentName;
+		setTitle("BattleBoats - " + matchTitle);
 		contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
+		
+		int rows = playerBoard.getHeight(),
+			cols = playerBoard.getWidth();
+		playerBoardUI = new GameBoardUI(rows, cols, playerBoard, false);
+		opposingBoardUI = new GameBoardUI(rows, cols);
 		createComponents();
 	}
 	
+	public void init() {
+		pack();
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //TODO show BattleBoatsUI on close
+		
+	}
 	private void createComponents() {
 		createPlayerScreen(1);
 		
@@ -68,12 +101,13 @@ public class GameUI extends JFrame {
 	}
 	
 	private void createPlayerScreen(int playerNumber) {
-		JPanel playerScreen = new JPanel();
+//		JPanel playerScreen = new JPanel();
 //		createTimer();
-		createBoards(game.getGameSettings().getPlayer1Name(), game.getGameSettings().getPlayer2Name());
+		createPanels();
 		createButtons();
 		
-		contentPane.add(playerScreen, "player"+playerNumber);
+//		contentPane.add(playerScreen, "player"+playerNumber);
+//		contentPane.add(playerScreen);
 	}
 	
 	private void createTimer() {
@@ -86,28 +120,46 @@ public class GameUI extends JFrame {
 		timerLabel.setEditable(false);
 		timerPane.add(label);
 		
-		timer = new Timer(1000, new TimerListener());
-		//timerPane.add(timer);
+//		timer = new Timer(1000, new TimerListener());
+//		timerPane.add(timer);
 		
 		contentPane.add(timerPane, BorderLayout.NORTH);
 	}
 	
-	private void createBoards(String player1, String player2) {
-		JPanel boardsPane = new JPanel();
-		JLabel label;
+	private void createPanels() {
+		JPanel boardsPane = new JPanel(),
+			   playerBoardPane = new JPanel(),
+			   opposingBoardPane = new JPanel();
+		JLabel label = new JLabel();
 		boardsPane.setLayout(new SpringLayout());
 		
-		JPanel player1Pane = new JPanel();
-		label = new JLabel(player1);
-		label.setFont(label.getFont().deriveFont(14.0f));
-		player1Pane.add(label);
-		boardsPane.add(player1Pane);
+//		Border borderStyle = BorderFactory.createLineBorder(Color.BLACK);
+		Border borderStyle = BorderFactory.createEmptyBorder();
+		TitledBorder title = BorderFactory.createTitledBorder(
+				borderStyle, playerName);
+		title.setTitleFont(label.getFont().deriveFont(14.0f));
+		title.setTitleJustification(TitledBorder.CENTER);
+		playerBoardUI.setBorder(title);
+		playerBoardPane.add(playerBoardUI);
+		boardsPane.add(playerBoardPane);
 		
-		JPanel player2Pane = new JPanel();
-		label = new JLabel(player2);
-		label.setFont(label.getFont().deriveFont(14.0f));
-		player2Pane.add(label);
-		boardsPane.add(player2Pane);
+		title = BorderFactory.createTitledBorder(
+				borderStyle, opponentName);
+		title.setTitleFont(label.getFont().deriveFont(14.0f));
+		title.setTitleJustification(TitledBorder.CENTER);
+		opposingBoardUI.setBorder(title);
+		opposingBoardUI.addPropertyChangeListener(new PropertyChangeListener() {
+
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		opposingBoardPane.add(opposingBoardUI);
+		//opposingBoardPane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		boardsPane.add(opposingBoardPane);
 		
 		SpringUtilities.makeCompactGrid(boardsPane,
                 1, 2, 	//rows, cols
@@ -118,22 +170,27 @@ public class GameUI extends JFrame {
 
 	private void createButtons() {
 		JPanel buttonPane = new JPanel();
+		buttonPane.setBorder(new EmptyBorder(0, 0, 10, 0));
 		JButton button;
 		
 		buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.X_AXIS));
 		buttonPane.setAlignmentY(RIGHT_ALIGNMENT);
 		
+		buttonPane.add(Box.createHorizontalGlue());
 		button = new JButton("Forfeit");
 		button.addActionListener(new ForfeitGameListener());
 		buttonPane.add(button);
+		buttonPane.add(Box.createHorizontalStrut(10));
 		
 		button = new JButton("Save Game");
 		button.addActionListener(new SaveGameListener());
 		buttonPane.add(button);
+		buttonPane.add(Box.createHorizontalStrut(10));
 		
 		button = new JButton("End Turn");
 		button.addActionListener(new NextTurnListener());
 		buttonPane.add(button);
+		buttonPane.add(Box.createHorizontalGlue());
 		
 		contentPane.add(buttonPane, BorderLayout.SOUTH);
 		
@@ -159,7 +216,14 @@ public class GameUI extends JFrame {
 	
 	private class ForfeitGameListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			// TODO
+			int opt = JOptionPane.showConfirmDialog(null,
+					"Are you sure you want to forfeit the game?",
+					"Forfeit Game?", JOptionPane.YES_NO_OPTION);
+			if (opt == 0) {
+				//TODO
+				// shut down server (if host)
+				// return to main menu
+			}
 		}
 	}
 	
